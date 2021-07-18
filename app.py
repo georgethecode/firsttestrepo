d37439414cf4770915ed7dfa4830675958087c6c
georgethecode <87601989+georgethecode@users.noreply.github.com>
2021-07-18 21:56:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,15 +158,13 @@ def error_occured():
 
     return error
 
-app.run(debug=False)
+if __name__ == "__main__":
+    app.run()
 
-# if __name__ == "__main__":
-#     app.run()
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5004))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     # Bind to PORT if defined, otherwise default to 5000.
+#     port = int(os.environ.get('PORT', 5004))
+#     app.run(host='0.0.0.0', port=port)
 
 
 
